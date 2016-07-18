5d860b7993651e9e7528ce4f7f6fdc24c02e35a4
Jonathan Keljo <jkeljo@fb.com>
2016-07-18 16:52:21 -0700

    Factor out global render method
    Reducing copy/paste in `index.jsx` because I'm about to add more code there.

--- src/main/js/index.jsx
+++ src/main/js/index.jsx
@@ -13,23 +13,23 @@ const holderDiv = document.createElement('div');
 holderDiv.classList.add('tracery-app-container');
 document.body.appendChild(holderDiv);
 
-ReactDOM.render(
-  // TODO: react-hot-loader@3.0.0-beta.2 seems to include a version of Redbox
-  // that doesn't like React 15. Once that's fixed, we can stop specifying it
-  // explicitly here and remove our dependency on redbox-react.
-  <AppContainer errorReporter={Redbox}>
-    <BrowserHost />
-  </AppContainer>,
-  holderDiv);
+function render(Host: ReactClass<*>) {
+  ReactDOM.render(
+    // TODO: react-hot-loader@3.0.0-beta.2 seems to include a version of Redbox
+    // that doesn't like React 15. Once that's fixed, we can stop specifying it
+    // explicitly here and remove our dependency on redbox-react.
+    <AppContainer errorReporter={Redbox}>
+      <Host />
+    </AppContainer>,
+      holderDiv);
+}
+
+render(BrowserHost);
 
 if (module.hot) {
   module.hot.accept('./BrowserHost', () => {
     /* eslint global-require: 0 */
     const NextHost = require('./BrowserHost').default;
-    ReactDOM.render(
-      <AppContainer>
-        <NextHost />
-      </AppContainer>,
-      holderDiv);
+    render(NextHost);
   });
 }
